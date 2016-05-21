687ae9d172a5fde16d0670f49b9401047987dc60
Nicolas Lepage <nicolas.lepage@zenika.com>
2016-05-21 16:35:34 +0200

    Déplacement des json de static/resources dans resources pour pouvoir faire du dynamique plus tard...

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/agiliquizz/static', express.static(__dirname + '/static'));
+app.use('/agiliquizz/resources', express.static(__dirname + '/resources'));
 
 app.get('/agiliquizz/*', function (req, res) {
     res.sendFile(__dirname + '/index.html');
